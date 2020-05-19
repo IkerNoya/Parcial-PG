f16db88b1105a6c795a53dc181bc474544d2d12d
Iker <ikernoya4@gmail.com>
2020-05-19 10:27:38 -0300

    El programa ahora muestra una ventana

--- Parcial/ejer2.cpp
+++ Parcial/ejer2.cpp
@@ -1,6 +1,24 @@
 #include "GLFW/glfw3.h"
 int main()
 {
-  
+    GLFWwindow* window;
+	if (!glfwInit())
+	{
+		return -1;
+	}
+	window = glfwCreateWindow(1280, 720, "Ejer2-Parcial", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1; 
+	}
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
     return 0;
 }
